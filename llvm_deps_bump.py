c92224c67595f5b5f45d967d3eee9960390635a3
Mark Seaborn <mseaborn@chromium.org>
2013-06-19 15:44:17 -0700

    llvm_deps_bump.py: Add "--component=clang" option for updating Clang rev instead

--- llvm_deps_bump.py
+++ llvm_deps_bump.py
@@ -11,9 +11,6 @@ import sys
 from deps_bump import ReadFile, WriteFile, GetDepsField, SetDepsField
 
 
-LLVM_DIR = 'pnacl/git/llvm'
-
-
 def GetCommandOutput(command, **kwargs):
   proc = subprocess.Popen(command, stdout=subprocess.PIPE, **kwargs)
   stdout = proc.communicate()[0]
@@ -22,20 +19,20 @@ def GetCommandOutput(command, **kwargs):
   return stdout
 
 
-def GetNewRev():
-  subprocess.check_call(['git', 'fetch'], cwd=LLVM_DIR)
+def GetNewRev(git_dir):
+  subprocess.check_call(['git', 'fetch'], cwd=git_dir)
   return GetCommandOutput(['git', 'rev-parse', 'origin/master'],
-                          cwd=LLVM_DIR).strip()
+                          cwd=git_dir).strip()
 
 
-def GetLog(rev1, rev2):
+def GetLog(git_dir, rev1, rev2):
   log_args = ['^' + rev1, rev2, '^origin/upstream/master']
   log_data = GetCommandOutput(['git', 'log', '--pretty=format:%h: (%ae) %s']
-                              + log_args, cwd=LLVM_DIR)
+                              + log_args, cwd=git_dir)
   authors_data = GetCommandOutput(['git', 'log', '--pretty=%ae']
-                                  + log_args, cwd=LLVM_DIR)
+                                  + log_args, cwd=git_dir)
   full_log = GetCommandOutput(['git', 'log', '--pretty=%B']
-                              + log_args, cwd=LLVM_DIR)
+                              + log_args, cwd=git_dir)
   log = '\n'.join(reversed(log_data.strip().split('\n'))) + '\n'
   authors = authors_data.strip().split('\n')
   bugs = []
@@ -66,8 +63,10 @@ def AssertNoUncommittedChanges():
 
 def Main(args):
   parser = optparse.OptionParser()
+  parser.add_option('-c', '--component', default='llvm', type='string',
+                    help='Subdirectory of pnacl/git/ to update DEPS from')
   parser.add_option('-r', '--revision', default=None, type='string',
-                    help='LLVM Git revision to use')
+                    help='Git revision to use')
   parser.add_option('-u', '--no-upload', action='store_true', default=False,
                     help='Do not run "git cl upload" (implies --no-try)')
   parser.add_option('-T', '--no-try', action='store_true', default=False,
@@ -76,17 +75,22 @@ def Main(args):
   if len(args) != 0:
     parser.error('Got unexpected arguments')
 
+  git_dir = os.path.join('pnacl/git', options.component)
+  component_name = {'llvm': 'LLVM',
+                    'clang': 'Clang'}[options.component]
+  deps_field = {'llvm': 'pnacl_llvm_rev',
+                'clang': 'clang_rev'}[options.component]
+
   AssertNoUncommittedChanges()
 
   new_rev = options.revision
   if new_rev is None:
-    new_rev = GetNewRev()
+    new_rev = GetNewRev(git_dir)
 
   subprocess.check_call(['git', 'fetch'])
   subprocess.check_call(['git', 'checkout', 'origin/master'])
 
   deps_file = 'pnacl/DEPS'
-  deps_field = 'pnacl_llvm_rev'
   deps_data = ReadFile(deps_file)
   old_rev = GetDepsField(deps_data, deps_field)
   if new_rev == old_rev:
@@ -94,9 +98,9 @@ def Main(args):
   deps_data = SetDepsField(deps_data, deps_field, new_rev)
   WriteFile(deps_file, deps_data)
 
-  msg_logs, authors, bugs = GetLog(old_rev, new_rev)
-  msg = 'PNaCl: Update LLVM revision in pnacl/DEPS'
-  msg += '\n\nThis pulls in the following LLVM changes:\n\n'
+  msg_logs, authors, bugs = GetLog(git_dir, old_rev, new_rev)
+  msg = 'PNaCl: Update %s revision in pnacl/DEPS' % component_name
+  msg += '\n\nThis pulls in the following %s changes:\n\n' % component_name
   msg += msg_logs
   msg += '\n'
   msg += bugs
@@ -106,7 +110,7 @@ def Main(args):
   cc_list = ', '.join(sorted(set(authors)))
   print 'CC:', cc_list
 
-  branch_name = 'llvm-deps-%s' % new_rev[:8]
+  branch_name = '%s-deps-%s' % (options.component, new_rev[:8])
   subprocess.check_call(['git', 'checkout', '-b', branch_name])
 
   if options.no_upload:
